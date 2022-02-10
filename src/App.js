e13d812ccbf5f963e0010032c21d1eacd03464df
Andrew Kessinger <andrew.kessinger@gmail.com>
2022-02-10 20:25:37 +0100

    CSS

--- src/App.js
+++ src/App.js
@@ -78,8 +78,8 @@ function App() {
   }
 
   return (
-    <div className='grid min-h-screen place-content-center bg-gray-200'>
-      <div className='w-[95vw] sm:w-[600px] rounded-lg border border-gray-400 bg-white'>
+    <div className='grid min-h-screen place-content-center bg-gray-200 overflow-y-hidden'>
+      <div className='w-[95vw] sm:w-[550px] rounded-lg border border-gray-400 bg-white'>
 
         {/* Search by name form */}
         <form action='#' className='m-2' id='searchByName'>
